4c3849461d41610993fa4d6d77df81003a1a68e9
Anas Juwaidi <anas.didi95@gmail.com>
2024-08-25 17:36:56 +0800

    FIX: Fix message

--- service-startall.sh
+++ service-startall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Stopping starting services..."
+echo "Starting all services..."
 
 echo "Starting edumgmt-server" && sudo systemctl restart edumgmt-server && \
 echo "Starting nginx-certbox" && sudo systemctl restart nginx-certbot
